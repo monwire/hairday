8cefc64b23acedc046ecc8a097500c04cd2f382e
monwire <monwire.dev@gmail.com>
2025-09-12 14:18:36 -0300

    Atualização da build

--- src/modules/schedules/load.js
+++ src/modules/schedules/load.js
@@ -1,10 +1,15 @@
+import { scheduleFetchByDay } from "../../services/schedule-fetch-by-day"
 import { hoursLoad } from "../form/hours-load"
 
 // Seleciona o input de data
 const selectedDate = document.getElementById("date")
 
-export function schedulesDay() {
+export async function schedulesDay() {
   const date = selectedDate.value
+
+  // Buscando os agendamentos na API
+  const dailySchedules = await scheduleFetchByDay({ date })
+
   // Renderiza as horas disponíveis.
   hoursLoad({ date })
 }
